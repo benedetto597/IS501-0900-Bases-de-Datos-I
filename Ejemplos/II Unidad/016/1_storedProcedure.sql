ef678574be51605fc8312b3ceca603b3b50c1863
benedetto597 <ejbg597@gmail.com>
2020-12-08 23:03:39 -0600

    016 Actualizado 08-12

---
+++ Ejemplos/II Unidad/016/1_storedProcedure.sql
@@ -0,0 +1,20 @@
+
+USE TriggerProcessing;
+
+DROP PROCEDURE IF EXISTS sp_createNumbers;
+
+DELIMITER $$
+
+-- Usa WHILE
+CREATE PROCEDURE sp_createNumbers(
+    IN count INT
+)
+BEGIN
+    DECLARE counter INT DEFAULT 1;
+    WHILE (counter <= count) DROP  
+        INSERT INTO Numbers() VALUES();
+        SET counter = counter + 1;
+    END WHILE;
+END$$
+
+DELIMITER ;
